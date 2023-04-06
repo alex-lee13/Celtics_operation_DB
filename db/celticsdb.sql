5f2cfb835826d7a5803f35855ef7bc02f765d43f
alex-lee13 <122317975+alex-lee13@users.noreply.github.com>
2023-04-06 12:08:35 -0400

    Update celticsdb.sql

--- db/celticsdb.sql
+++ db/celticsdb.sql
@@ -254,6 +254,14 @@ INSERT INTO PlayerAtGames VALUES(7, 'Jaylen', 'Brown', 01234, 5, 12, 13, 31, 4);
 INSERT INTO ScoutingReports VALUES(01234, 6, 8, 'celtics up', 'pick and roll', 'backdoor');
 INSERT INTO ScoutingReports VALUES(02318, 7, 6, 'celtics up', 'pick and roll', 'backdoor');
 
+INSERT INTO AiredGames values (1, 01234, 500000, 12345678.99);
+INSERT INTO AiredGames values (2, 01234, 250000, 1234567.89);
+
+INSERT INTO Broadcasters values (1, 'Telemundo', 'USA', 'English');
+INSERT INTO Broadcasters values (2, 'ESPN', 'USA', 'English');
+
+INSERT INTO PracticePlan values (1, False, 'Shooting', 'Shooting Machine');
+INSERT INTO PracticePlan values (2, True, 'Defense', 'Cones');
 
 SELECT * FROM PlayerAtGames;
 
