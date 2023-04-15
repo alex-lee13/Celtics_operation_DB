b4eda75bf9d476ae51693db4d94c9d0fd21ef9c7
MichaelRemijan <gapudremijan.m@northeastern.edu>
2023-04-15 16:18:12 -0400

    added /practices

--- flask-app/src/coaches/coaches.py
+++ flask-app/src/coaches/coaches.py
@@ -56,4 +56,29 @@ def get_player(p_number):
        json_data.append(dict(zip(column_headers, row)))
 
    return jsonify(json_data)
+
+@coaches.route('/practices', methods = ['GET'])
+def get_all_practices():
+    # get a cursor object from the database
+   cursor = db.get_db().cursor()
+    
+   # use cursor to query the database for a list of products
+   cursor.execute('SELECT * from Practices')
+
+   # grab the column headers from the returned data
+   column_headers = [x[0] for x in cursor.description]
+
+   # create an empty dictionary object to use in 
+   # putting column headers together with data
+   json_data = []
+
+   # fetch all the data from the cursor
+   theData = cursor.fetchall()
+
+   # for each of the rows, zip the data elements together with
+   # the column headers. 
+   for row in theData:
+       json_data.append(dict(zip(column_headers, row)))
+
+   return jsonify(json_data)
   
