2b9090fb9808f94889aad188f65496bfc999a514
MichaelRemijan <gapudremijan.m@northeastern.edu>
2023-04-18 12:05:32 -0400

    added get scouting reports

--- flask-app/src/coaches/coaches.py
+++ flask-app/src/coaches/coaches.py
@@ -188,6 +188,31 @@ def update_scoutingreport(gameID):
     db.get_db().commit()
     return 'Success!'
 
+@coaches.route('/scoutingreports', methods = ['GET'])
+def get_all_scouting_reports():
+    # get a cursor object from the database
+   cursor = db.get_db().cursor()
+    
+   # use cursor to query the database for a list of products
+   cursor.execute('SELECT * from ScoutingReports')
+
+   # grab the column headers from the returned data
+   column_headers = [x[0] for x in cursor.description]
+
+   # create an empty dictionary object to use in 
+   # putting column headers together with data
+   json_data = []
+
+   # fetch all the data from the cursor
+   theData = cursor.fetchall()
+
+   # for each of the rows, zip the data elements together with
+   # the column headers. 
+   for row in theData:
+       json_data.append(dict(zip(column_headers, row)))
+
+   return jsonify(json_data)
+
 # Post a new scouting report for a new game (game needs to be created separately)
 @coaches.route('/games', methods = ['POST'])
 def post_scoutingreport():
